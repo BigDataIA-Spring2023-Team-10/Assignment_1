1a1154d996b752feaa146f9cc4355906183691eb
snehashislenka <lenka.s@northeastern.edu>
2023-02-10 04:13:00 -0500

    file updates

--- streamlit/pages/NEXRAD_SITES.py
+++ streamlit/pages/NEXRAD_SITES.py
@@ -1,8 +1,21 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import pydeck as pdk
-import sqlite_main as db_methods
+
+import importlib.util
+import os
+
+current_directory = os.getcwd()
+
+module_directory = os.path.abspath(os.path.join(current_directory, 'src', 'data'))
+module_path = os.path.join(module_directory, 'sqlite_main.py')
+ops_path = os.path.join(module_directory, 'backend_ops.py')
+
+spec = importlib.util.spec_from_file_location("sqlite_main", module_path)
+spec_ops = importlib.util.spec_from_file_location("backend_ops", ops_path)
+db_methods = importlib.util.module_from_spec(spec)
+ops = importlib.util.module_from_spec(spec_ops)
+spec.loader.exec_module(db_methods)
+spec_ops.loader.exec_module(ops)
 
 st.title('Locations of all NEXRAD sites')
 
@@ -12,8 +25,8 @@ sites_data.head()
 
 sitedf = pd.DataFrame(
     {
-        "lat": sites_data['Latitude'],
-        "lon": sites_data['Longitude']
+        "lat": sites_data['Lat'],
+        "lon": sites_data['Lon']
     }
 )
 
